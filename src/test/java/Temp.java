bdaa6585a9d5f8374672e0ef2993f787d8f7d1c5
Eric Zong <ericzonglu@126.com>
2018-07-06 21:06:34 +0800

    Update

---
+++ src/test/java/Temp.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+/**
+ * @author zonglu
+ */
+public class Temp {
+	@Test
+	public void test()
+	{
+		Integer[] is = {1,2,3};
+		System.out.println(is.getClass().getTypeName());
+	}
+}
