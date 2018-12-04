3cc82c11bdab7f60ffd7238a8bee07f89faeee38
Eric Zong <ericzonglu@126.com>
2018-12-04 19:31:36 +0800

    Set

--- src/main/java/com/ericzong/java/sample/api/collection/SetTest.java
+++ src/main/java/com/ericzong/java/sample/api/collection/SetTest.java
@@ -3,14 +3,14 @@ package com.ericzong.java.sample.api.collection;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.*;
 
 public class SetTest {
 
     @Test
     public void testNull() {
         HashSet<Object> set = new HashSet<>();
+        Assert.assertFalse(set.contains(null));
         set.add(null);
         Assert.assertTrue(set.contains(null));
     }
@@ -31,6 +31,59 @@ public class SetTest {
         e1.setAge(31);
         set.add(e1);
         Assert.assertEquals(set.size(), 2);
+    }
+
+    @Test
+    public void testRemoveWhileForEach() {
+        HashSet<String> set = new HashSet<>();
+        set.add("1");
+        set.add("2");
+        set.add("3");
+
+        for (String s: set) {
+            if(s.equals("3")) {
+                set.remove(s);
+            }
+        }
+        Assert.assertEquals(set.size(), 2);
+    }
+
+    @Test
+    public void testIteratorRemove() {
+        HashSet<String> set = new HashSet<>();
+        set.add("1");
+        set.add("2");
+        set.add("3");
+
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+        }
+        Assert.assertTrue(set.isEmpty());
+    }
+
+    @Test(expectedExceptions = ClassCastException.class)
+    public void testTreeSetNoComparable() {
+        TreeSet<VolatileElement> set = new TreeSet<>();
+        set.add(new VolatileElement("Eric", 30));
+    }
+
+    @Test
+    public void testTreeSetComparator() {
+        TreeSet<VolatileElement> set = new TreeSet<>((o1, o2) -> {
+            if(o1.name.compareTo(o2.name) > 0) {
+                return 1;
+            } else if(o1.name.compareTo(o2.name) < 0) {
+                return -1;
+            } else {
+                return Integer.valueOf(o1.age).compareTo(o2.age);
+            }
+        });
+        set.add(new VolatileElement("Eric", 31));
+        set.add(new VolatileElement("Eric", 30));
+        Assert.assertEquals(set.first().age, 30);
+        Assert.assertEquals(set.last().age, 31);
     }
 
     static class WrongElement {
