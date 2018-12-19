2bf993ff7010cc89f2966cae7350eb9eb7da37f4
Eric Zong <ericzonglu@126.com>
2018-12-19 17:34:12 +0800

    List

--- src/main/java/com/ericzong/java/sample/api/collection/ListTest.java
+++ src/main/java/com/ericzong/java/sample/api/collection/ListTest.java
@@ -1,10 +1,31 @@
 package com.ericzong.java.sample.api.collection;
 
+import org.testng.annotations.Test;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListTest {
-    public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(Integer.MAX_VALUE - 8);
-        list.size();
+
+    @Test
+    public void testNew() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+    }
+
+    @Test
+    public void testNewEmpty() {
+        ArrayList<WatchData> list = new ArrayList<>(0);
+        list.add(new WatchData());
+    }
+
+    @Test
+    public void testNewEmptyCollection() {
+        ArrayList<WatchData> list = new ArrayList<>(List.of(new WatchData()));
+        list.add(new WatchData());
+    }
+
+    public static class WatchData {
+
     }
 }
